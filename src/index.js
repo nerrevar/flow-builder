9460ef3133c249f9c9a37b2f4e1725b024288435
nerrevar <nerrevar12@yandex.ru>
2021-09-13 16:10:15 +0300

    base three config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-import '@/style.sass'
+import * as THREE from 'three'
+
+const flowBuilder = {
+  renderer: null,
+  scene: null,
+  camera: null,
+
+  init() {
+    this.renderer = new THREE.WebGLRenderer({ antialias: true })
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    window.addEventListener(
+      "resize",
+      () => this.renderer.setSize(window.innerWidth, window.innerHeight)
+    )
+    document.querySelector("#three-entrypoint").appendChild(this.renderer.domElement)
+
+    this.scene = new THREE.Scene()
+    this.scene.background = new THREE.Color(0x606060)
+
+    this.camera = new THREE.OrthographicCamera(-10, 10, -10, 10, 1, 50)
+    this.scene.add(this.camera)
+
+    const light = new THREE.AmbientLight(0xffffff)
+    this.scene.add(light)
+
+    this.animate()
+  },
+
+  animate() {
+    this.renderer.render(this.scene, this.camera)
+    requestAnimationFrame(() => this.animate())
+  },
+}
+
+flowBuilder.init()
--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,7 @@ module.exports = {
     hot: true
   },
   resolve: {
+    modules: ['node_modules'],
     alias: {
       '@': './'
     }
